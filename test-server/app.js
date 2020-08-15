7b2fa50965a321185f989bda7fed09c3861e26d2
adela <adelabbok@gmail.com>
2020-08-15 11:03:59 +0900

    add: test-server 수정

--- test-server/app.js
+++ test-server/app.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
-var cors = require("cors");
+const cors = require("cors");
 const port = 4000;
+const bodyParser = require("body-parser");
 
+const serverData = [
+	{
+		id: 1,
+		username: "서버데이터_김코딩",
+		email: "dd@dd",
+		time: "08:23:00",
+		day: "2020.08.13",
+	},
+	{
+		id: 2,
+		username: "서버데이터_박코딩",
+		email: "ff@ff",
+		time: "03:50:08",
+		day: "2020.08.14",
+	},
+];
+
+app.use(bodyParser.json());
 app.use(
 	cors({
 		origin: ["http://localhost:3000"],
@@ -12,16 +31,29 @@ app.use(
 );
 
 app.get("/", (req, res) => {
+	//
 	console.log("get요청", req);
 	res.send("Hello World! test-server");
 });
 
 app.post("/signin", (req, res) => {
-	console.log("post요청", req);
-	res.send("Hello World! test-server>>>>");
+	// console.log("로그인post요청", req.body);
+	let findData = serverData.filter((user) => user.email === req.body.email);
+	// console.log("email같은유저찾기", findData);
+	res.send(findData);
 });
 
-// app.get("/signin", (req, res) => {});
+app.post("/signup", (req, res) => {
+	console.log("회원가입post요청", req.body);
+	// let findData = serverData.filter((user) => user.email === req.body.email);
+	// if (findData) {
+	// 	res.status(409).send("Already exists");
+	// } else if (!findData) {
+	// serverData.push(req.body); // 신규유저 생성
+	// console.log(serverData);
+	res.send(req.body);
+	// } // 가입된 유저에대한 뭔가를보내주고싶은데 자꾸409에러남
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
